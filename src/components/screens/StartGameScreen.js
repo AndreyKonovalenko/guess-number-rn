6862e8778dd26e422f11a1ecd0e10d986b3a2a5d
AndreyKonovalenko <andreinew@mail.ru>
2019-08-24 14:14:46 +0300

    Adde new component Card which holds style

--- src/components/screens/StartGameScreen.js
+++ src/components/screens/StartGameScreen.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
-
+import Card from '../Card';
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start New Game</Text>
-        <View style={styles.inputContainer}>
-          <Text>Select a Number</Text>
-          <TextInput />
-          <View style={styles.buttonContainer}>
-            <Button title='Reset' onPress={()=>{}}/>
-            <Button title='Confirm' onPress={()=>{}}/>
-          </View>
+      <Card style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title='Reset' onPress={() => {}} />
+          <Button title='Confirm' onPress={() => {}} />
         </View>
+      </Card>
     </View>
   );
 };
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   buttonContainer: {
     flexDirection: 'row',
