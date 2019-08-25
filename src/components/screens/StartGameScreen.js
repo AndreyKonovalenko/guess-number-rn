480ee58cb79d0a0e7b6ac53d6850bb433d256f6d
AndreyKonovalenko <andreinew@mail.ru>
2019-08-25 13:37:29 +0300

    Added custom input component

--- src/components/screens/StartGameScreen.js
+++ src/components/screens/StartGameScreen.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 import Card from '../Card';
 import Colors from '../../constants/colors';
+import Input from '../Input';
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start New Game</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType='number-pad'
+          maxLength={2}
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
             <Button title='Reset' color={Colors.secondary} onPress={() => {}} />
@@ -49,6 +57,10 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: '80%',
     alignItems: 'center'
+  },
+  input: {
+    width: 50,
+    textAlign: 'center'
   }
 });
 
