8999578564256c12aaa0b61704c78968f110ffc9
EC2 Default User <ec2-user@ip-172-31-34-228.us-east-2.compute.internal>
2019-08-27 11:27:39 +0000

    Added local storing image and style it

--- src/components/screens/GameOverScreen.js
+++ src/components/screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 import BodyText from '../BodyText';
 import TitleText from '../TitleText';
 
@@ -7,6 +7,9 @@ const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('../../../assets/success.png')} resizeMode='cover'/>
+      </View>
       <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number was: {props.userChoice}</BodyText>
       <Button title='NEW GAME' onPress={props.onRestart} />
@@ -18,7 +21,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  }
+  },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 300,
+    height: 300,
+    overflow: 'hidden',
+    marginVertical: 30
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  },
 });
 
 export default GameOverScreen;
