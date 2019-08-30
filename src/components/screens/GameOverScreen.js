da0314c1fdc40732537608bca734012401fa44c6
EC2 Default User <ec2-user@ip-172-31-34-228.us-east-2.compute.internal>
2019-08-30 09:59:35 +0000

    enable ladscape orientation

--- src/components/screens/GameOverScreen.js
+++ src/components/screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, Dimensions, ScrollView } from 'react-native';
 import BodyText from '../BodyText';
 import TitleText from '../TitleText';
 import Colors from '../../constants/colors';
@@ -7,6 +7,7 @@ import MainButton from '../MainButton';
 
 const GameOverScreen = props => {
   return (
+    <ScrollView>
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
@@ -28,6 +29,7 @@ const GameOverScreen = props => {
       </View>
       <MainButton onUserPress={props.onRestart}>NEW GAME</MainButton>
     </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
@@ -37,13 +39,13 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   imageContainer: {
-    borderRadius: 150,
     borderWidth: 3,
     borderColor: 'black',
-    width: 300,
-    height: 300,
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: Dimensions.get('window').width * 0.7 / 2,
     overflow: 'hidden',
-    marginVertical: 30
+    marginVertical: Dimensions.get('window').height / 30
   },
   image: {
     width: '100%',
@@ -55,11 +57,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15
+    marginVertical: Dimensions.get('window').height / 60
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 20
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20
   }
 });
 
